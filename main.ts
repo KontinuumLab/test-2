b0853e0658b1e765fbffe1e3d9d1a57a7291a87b
KontinuumLab <jepperasmussen.com@gmail.com>
2022-09-30 13:34:15 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
-input.onGesture(Gesture.Shake, function () {
-    music.playMelody("E F G - G C - E ", 120)
-})
-input.onButtonPressed(Button.B, function () {
-    music.playMelody("- - - - - - - - ", 120)
+let SubeBaja = 0
+let x = 120
+basic.forever(function () {
+    if (x <= 120) {
+        SubeBaja = 1
+    }
+    if (x >= 250) {
+        SubeBaja = 0
+    }
+    if (SubeBaja == 1) {
+        x = x + 1
+    }
+    if (SubeBaja == 0) {
+        x = x - 1
+    }
+    music.ringTone(x)
 })
